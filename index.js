994d22c6664488c10f2c99685b77c18aa08c63aa
Luciano Pena <lucianonp23@gmail.com>
2023-01-27 15:08:20 +0000

    botão dinamico até a pagina de descricao da pergunta

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.get("/pergunta/:id", function(req,res){
         where:{id:id}
     }).then(pergunta=>{
         if(pergunta !=undefined){
-            res.render("pergunta")
+            res.render("pergunta",{
+                pergunta:pergunta
+            })
         }else{
             res.redirect("/");
         }
